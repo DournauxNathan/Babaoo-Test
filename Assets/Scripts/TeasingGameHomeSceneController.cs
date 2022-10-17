06415a2b750d4d105ee1526ab61f2e04144a5620
DournauxNathan <n.dournaux@rns.ecolescreatives.com>
2022-10-17 08:22:55 +0200

    Game Scene
    Add a New Scene "Game".
    - Navigate between the Home & Game scene with button & TeasingGameHomeSceneController script

--- Assets/Scripts/TeasingGameHomeSceneController.cs
+++ Assets/Scripts/TeasingGameHomeSceneController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using SceneTransitionSystem;
+using UnityEngine.SceneManagement;
 
 
 namespace TeasingGame
@@ -29,7 +29,7 @@ public class TeasingGameHomeSceneController : MonoBehaviour
 
    public void GoToGameScene()
     {
-        STSSceneManager.LoadScene(SceneForButton.ToString());
+        SceneManager.LoadScene(SceneForButton.ToString());
     }
 }
 }
